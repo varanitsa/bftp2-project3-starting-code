a2bbc71101ce3473a768e6f41b27dbf613ba9112
Javi <javi@jsrois.net>
2021-11-23 14:11:28 +0100

    Commit inicial

---
+++ src/main/java/net/filmcity/app/Bftp2Project3TemplateApplication.java
@@ -0,0 +1,13 @@
+package net.filmcity.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bftp2Project3TemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Bftp2Project3TemplateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/filmcity/app/Bftp2Project3TemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package net.filmcity.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bftp2Project3TemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
