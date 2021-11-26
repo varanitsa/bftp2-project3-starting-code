bf9bda2644f357bfc5f2e1500d367fff48d47295
AnaCasasC <cervelloana@gmail.com>
2021-11-26 12:08:37 +0100

    comentario

--- src/main/java/net/filmcity/app/controllers/MoviesController.java
+++ src/main/java/net/filmcity/app/controllers/MoviesController.java
@@ -4,6 +4,7 @@ import net.filmcity.app.domain.Movie;
 import net.filmcity.app.repositories.MovieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -22,4 +23,9 @@ public class MoviesController {
     public List<Movie> allMovies() {
         return movieRepository.findAll();
     }
+
+   /* @GetMapping("/movies/{index}")
+    public Movie findMovie(@PathVariable int index) {
+        return movieRepository.findByIndex(index).orElseThrow(MovieNotFoundException::new);
+    }*/
 }
