30866e4b8785d7ce34b8a8aa3740c5b7ac1ed995
19valentinavergara98 <valentina.98.3440@gmail.com>
2021-11-29 12:16:38 +0100

    delete

---
+++ src/main/java/net/filmcity/app/controllers/MovieNotFoundException.java
@@ -0,0 +1,13 @@
+package net.filmcity.app.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "movie not found")
+public class MovieNotFoundException extends RuntimeException {
+}
+
+
+
+
+
