0f91df6e0ba4c0ad288f7478e46327648989e2dd
AnaCasasC <cervelloana@gmail.com>
2021-12-01 09:40:16 +0100

    iniciando con los tests

--- src/test/java/net/filmcity/app/IntegrationTests.java
+++ src/test/java/net/filmcity/app/IntegrationTests.java
@@ -2,13 +2,13 @@ package net.filmcity.app;
 
 import net.filmcity.app.domain.Movie;
 import net.filmcity.app.repositories.MovieRepository;
-import org.junit.jupiter.api.AfterEach;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
